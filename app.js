2ff44abe97d182c05dd21d36c565010c65d9b341
phillip98park <angha54321@gmail.com>
2023-11-01 23:24:40 +0900

    js 기초문법

---
+++ app.js
@@ -0,0 +1,7 @@
+let age=32;
+let userName='phillip';
+let hobbies = ['sports', 'cooking','reading'];
+let job = { title : 'Dev', place : 'busan', salary : 50000 };
+let jobTitle = 'Dev';
+
+alert(hobbies[1]);
