3c25df9eddc2f448e0d3f821d11cdc2829108deb
phillip98park <angha54321@gmail.com>
2023-11-01 23:27:25 +0900

    day 30 : js 기초문법

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let age=32;
 let userName='phillip';
 let hobbies = ['sports', 'cooking','reading'];
-let job = { title : 'Dev', place : 'busan', salary : 50000 };
-let jobTitle = 'Dev';
+let job = { 
+    title : 'Dev', 
+    place : 'busan', 
+    salary : 50000 
+};
 
 alert(hobbies[1]);
