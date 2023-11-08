d4d1fdd086ced7e66f748b2e868db3a7af70caca
phillip98park <angha54321@gmail.com>
2023-11-09 02:34:14 +0900

    day32 - js기초연습

---
+++ exercise.js
@@ -0,0 +1,38 @@
+// 자바스크립트에 주석을 추가하는 방법입니다
+// 주석은 실행되지 않습니다 - 이것들은 단지 여러분 또는 다른 개발자에게 추가 정보를 제공하기 위한 것입니다
+
+// 연습 시간!
+
+// 1) 세 개의 새로운 변수를 생성합니다:
+// - 여러분이 선택한 온라인 강좌의 이름을 저장하는 변수
+// - 그 강좌의 가격을 저장하는 변수
+// - 이 강좌를 들을 때 가지는 세 가지 주요 목표를 저장하는 변수
+// 2) 세 변수의 값을 출력하세요 ("alert" 사용)
+// 3) 세 변수를 "그룹화"하여 그들의 값을 여전히 출력해보세요
+// 4) "주요 목표" 변수에서 두 번째 요소를 출력하세요
+
+//1
+// let onlineLecture = "web";
+// let onlineLecturePrice = "12000";
+// let onlineLectureGoal = ["실력", "코딩", "개념"];
+
+//2
+//alert(onlineLecture);
+//alert(onlineLecturePrice);
+//alert(onlineLectureGoal);
+
+//3
+let online = {
+  lectcure: "web",
+  price: "12000",
+  goal: ["실력", "코딩", "개념"],
+};
+
+alert(online.lectcure);
+alert(online.price);
+alert(online.goal);
+
+//4
+alert(online.goal[1]);
+
+
